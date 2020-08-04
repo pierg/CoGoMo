19600b7e64da63349f4f9f1ffaa83dc861627579
pierg <nmall843r>
2020-08-04 11:42:23 +0200

    fix

--- scripts/v5/crome_specifications.py
+++ scripts/v5/crome_specifications.py
@@ -172,38 +172,38 @@ def get_inputs():
                 Patrolling([ap["cl"]["care_center"]])
             ])]
         ),
-        # CGTGoal(
-        #     name="serve-pharmacy",
-        #     description="serve pharmacy during the day",
-        #     context=ap["ct"]["day"] & ap["cl"]["pharmacy"],
-        #     contracts=[PContract([
-        #         DelayedReaction(
-        #             trigger=ap["s"]["get_med"],
-        #             reaction=ap["a"]["give_med"])
-        #     ])]
-        # ),
-        # CGTGoal(
-        #     name="welcome-patients",
-        #     description="welcome patients at their arrival and check their temperature",
-        #     context=[ap["ct"]["day"] & ap["cl"]["entrance"] & ap["ci"]["mild"],
-        #              ap["ct"]["day"] & ap["cl"]["entrance"] & ap["ci"]["severe"]],
-        #     contracts=[PContract([
-        #         PromptReaction(
-        #             trigger=ap["s"]["human_entered"],
-        #             reaction=ap["a"]["welcome_patient"])
-        #     ])]
-        # ),
-        # CGTGoal(
-        #     name="low-battery",
-        #     description="always go the charging point and contact the main station when the battery is low",
-        #     contracts=[PContract([
-        #         Recurrence_P_between_Q_and_R(
-        #             q=ap["s"]["low_battery"],
-        #             p=ap["l"]["charging"],
-        #             r=ap["s"]["full_battery"]
-        #         )
-        #     ])]
-        # )
+        CGTGoal(
+            name="serve-pharmacy",
+            description="serve pharmacy during the day",
+            context=ap["ct"]["day"] & ap["cl"]["pharmacy"],
+            contracts=[PContract([
+                DelayedReaction(
+                    trigger=ap["s"]["get_med"],
+                    reaction=ap["a"]["give_med"])
+            ])]
+        ),
+        CGTGoal(
+            name="welcome-patients",
+            description="welcome patients at their arrival and check their temperature",
+            context=[ap["ct"]["day"] & ap["cl"]["entrance"] & ap["ci"]["mild"],
+                     ap["ct"]["day"] & ap["cl"]["entrance"] & ap["ci"]["severe"]],
+            contracts=[PContract([
+                PromptReaction(
+                    trigger=ap["s"]["human_entered"],
+                    reaction=ap["a"]["welcome_patient"])
+            ])]
+        ),
+        CGTGoal(
+            name="low-battery",
+            description="always go the charging point and contact the main station when the battery is low",
+            contracts=[PContract([
+                Recurrence_P_between_Q_and_R(
+                    q=ap["s"]["low_battery"],
+                    p=ap["l"]["charging"],
+                    r=ap["s"]["full_battery"]
+                )
+            ])]
+        )
     ]
 
     """Instantiating a Library of Goals"""

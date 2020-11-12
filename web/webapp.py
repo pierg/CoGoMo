2953fcbc815b78209dce08b867e66398607dfaf8
pier <piergiuseppe.mallozzi@gmail.com>
2020-11-12 14:01:39 +0100

    commit

--- web/webapp.py
+++ web/webapp.py
@@ -3,5 +3,5 @@ import os
 from web import *
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 80))
     app.run(host='0.0.0.0', port=port, debug=True)
